0b653e1dc87357be0cd570e0705932c3f53ab50e
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-24 23:00:01 +0300

    Install project dependencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import Login from './components/Login';
+import store from './redux/configureStore';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+const { user } = store.getState();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        if (user)
+        {' '}
+        <App />
+        {' '}
+        else
+        {' '}
+        <Login />
+      </Router>
+    </Provider>
   </React.StrictMode>,
 );
 
