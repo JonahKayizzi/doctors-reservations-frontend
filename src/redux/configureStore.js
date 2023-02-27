5cdf43f83575414fa3213281b541c24a8b03cc00
cvalencia1991 <cesar4a6z@gmail.com>
2023-02-27 14:16:54 -0500

    Add implementation doctors with slices

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { configureStore } from '@reduxjs/toolkit';
 import doctorReducer from './doctors/doctorsSlice';
 
@@ -5,6 +6,17 @@ const store = configureStore({
   reducer: {
     doctorReducer,
   },
+=======
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import doctorsReducer from './Doctors/doctor';
+
+const rootReducer = combineReducers({
+  doctors: doctorsReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+>>>>>>> e6849fe (Add implementation doctors with slices)
 });
 
 export default store;
