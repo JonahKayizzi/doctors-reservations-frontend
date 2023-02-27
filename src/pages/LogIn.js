c9df5dc209167029bdb9753e0321c648bb8d48b9
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-27 23:27:19 +0300

    Create handle login and onChange methods for the login form

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -3,6 +3,18 @@ import { useDispatch } from 'react-redux';
 import { fetchUser } from '../redux/users';
 
 const LogIn = () => {
+  const [username, setUsername] = useState('');
+  const dispatch = useDispatch();
+
+  const onChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(fetchUser(username));
+  };
+
   return (
     <form onSubmit={handleLogin}>
       <input
