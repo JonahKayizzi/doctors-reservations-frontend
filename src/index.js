c753513be54682334ff897ff89e5d1e9208defac
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-27 19:44:50 +0300

    Implement authentication through the index page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import LogIn from './pages/LogIn';
@@ -9,12 +8,9 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const { users } = store.getState().users;
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        if (user) <App /> else <Login />
-      </Router>
-    </Provider>
+    <Provider store={store}>{users ? <App /> : <LogIn />}</Provider>
   </React.StrictMode>
 );
