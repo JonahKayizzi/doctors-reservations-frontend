a2a48de3382b0ed2908ab4fcc5169f6ffe133484
cvalencia1991 <cesar4a6z@gmail.com>
2023-02-28 17:19:23 -0500

    Add Styles Add Doctor Form

--- src/pages/AddDoctor.js
+++ src/pages/AddDoctor.js
@@ -27,15 +27,17 @@ const AddDoctor = () => {
   };
 
   return (
-    <div className="w-3/4 flex flex-col items-center justify-center">
-      <form className="flex flex-col" onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="Doctor Name" onChange={handleChange} required className="border rounded-lg py-2 px-3 mb-4" />
-        <input type="text" name="speciality" placeholder="Speciality" onChange={handleChange} required className="border rounded-lg py-2 px-3 mb-4" />
-        <textarea type="text" name="description" placeholder="Description" onChange={handleChange} className="border rounded-lg py-2 px-3 mb-4" />
-        <input type="date" name="graduation" onChange={handleChange} required className="border rounded-lg py-2 px-3 mb-4" />
-        <input type="text" name="image" placeholder="Add Image Link" onChange={handleChange} required className="border rounded-lg py-2 px-3 mb-4" />
-        <button type="submit" className="bg-lime-400 hover:bg-lime-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Add New Doctor</button>
-      </form>
+    <div className="w-3/4 flex flex-col items-center justify-center bg-lime-400 ">
+      <div className="flex flex-row justify-center bg-white w-80 h-96 p-3 rounded ">
+        <form className="flex flex-col" onSubmit={handleSubmit}>
+          <input type="text" name="name" placeholder="Doctor Name" onChange={handleChange} required className="border rounded-lg py-2 px-3 mb-4" />
+          <input type="text" name="speciality" placeholder="Speciality" onChange={handleChange} required className="border rounded-lg py-2 px-3 mb-4" />
+          <textarea type="text" name="description" placeholder="Description" onChange={handleChange} className="border rounded-lg py-2 px-3 mb-4" />
+          <input type="date" name="graduation" onChange={handleChange} required className="border rounded-lg py-2 px-3 mb-4" />
+          <input type="text" name="image" placeholder="Add Image Link" onChange={handleChange} required className="border rounded-lg py-2 px-3 mb-4" />
+          <button type="submit" className="bg-lime-400 hover:bg-lime-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Add New Doctor</button>
+        </form>
+      </div>
     </div>
   );
 };
