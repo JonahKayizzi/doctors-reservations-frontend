5b7ac9d1f24f8f9ce736929ee2b23e63269db85c
Camilovelag <camilo.vela.galindo@gmail.com>
2023-02-28 10:44:12 -0500

    Add attributes to DoctorBrief

--- src/components/DoctorBrief.js
+++ src/components/DoctorBrief.js
@@ -2,11 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const DoctorBrief = (props) => {
-  const { id, name } = props;
+  const {
+    id, name, image, speciality, description,
+  } = props;
 
   return (
     <div id={id}>
+      <img src={image} alt="doctor" />
       <h2>{name}</h2>
+      <h3>{speciality}</h3>
+      <p>{description}</p>
     </div>
   );
 };
@@ -14,6 +19,9 @@ const DoctorBrief = (props) => {
 DoctorBrief.propTypes = {
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  speciality: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 };
 
 export default DoctorBrief;
--- src/pages/Doctors.js
+++ src/pages/Doctors.js
@@ -2,24 +2,50 @@ import React from 'react';
 import DoctorBrief from '../components/DoctorBrief';
 
 const Doctors = () => {
-  const initialState = [
+// TBD: Use useSelect method to assign doctors value from reducer's state instead.
+  const doctors = [
     {
-      id: 1,
-      name: "Charles O'neil",
+      name: 'Steven Hobb',
+      speciality: 'Dentist',
+      graduation: '2001-01-30',
+      description: 'Top dentist in the city of New York. Come get your dentals corrected in a single appointment. ',
+      image: 'https://static9.depositphotos.com/1037778/1113/i/600/depositphotos_11138153-stock-photo-mature-dentist-surgeon-at-office.jpg',
+    },
+    {
+      name: 'Peter Omaha',
+      speciality: 'General Practice',
+      graduation: '05/02/2010',
+      description: 'Top dentist in the city of New York. Come get your dentals corrected in a single appointment. ',
+      image: 'https://t4.ftcdn.net/jpg/03/05/41/27/360_F_305412791_XRNiWaFCREjLLpSQfj0e736foBoYXXYv.jpg',
     },
   ];
 
-  const doctorsList = initialState.map((doctor) => (
-    <DoctorBrief
-      key={doctor.id}
-      id={doctor.id}
-      name={doctor.name}
-    />
+  const doctorsList = doctors.map((doctor) => (
+    <li key={doctor.id} style={{ width: '200px' }}>
+      <DoctorBrief
+        id={doctor.id}
+        name={doctor.name}
+        image={doctor.image}
+        speciality={doctor.speciality}
+        description={doctor.description}
+      />
+      <div>Space for social networks</div>
+      <br />
+      <br />
+    </li>
   ));
 
   return (
     <div>
-      {doctorsList}
+      <div>
+        <h1>Our doctors</h1>
+        <p>Check out between the most selected list of doctors around the country</p>
+      </div>
+      <br />
+      <br />
+      <ul>
+        {doctorsList}
+      </ul>
     </div>
   );
 };
