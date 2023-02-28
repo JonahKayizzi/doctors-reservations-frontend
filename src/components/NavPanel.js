f1c44f4735348e4877bcbb78434bff46cacb3762
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-02-28 17:42:53 -0300

    Add logo and alter navpanel to fit it in

--- src/components/NavPanel.js
+++ src/components/NavPanel.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
 const NavPanel = () => {
   const inactive = 'h-14 text-sm md:text-lg md:h-auto font-semibold md:font-bold py-2 flex items-center justify-center hover:bg-lime-600 hover:text-white';
   const activeClassName = `text-white active-green ${inactive}`;
   return (
-    <div className="flex flex-wrap h-full w-1/4">
-      <img src="#" alt="logo" className="w-full" />
-      <div className="flex flex-col justify-self-center justify-center w-full">
+    <div className="flex flex-col h-full w-1/4 md:w-48">
+      <div className="h-1/8 w-full p-1">
+        <img src={logo} alt="logo" className="w-full h-full md:h-full" />
+      </div>
+      <div className="flex flex-col justify-center w-full">
         <NavLink
           className={({ isActive }) => (isActive ? activeClassName : inactive)}
           to="/"
