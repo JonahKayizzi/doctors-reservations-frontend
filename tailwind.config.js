cf5fb67ffc612423ba30f97645141e5132e139d6
cvalencia1991 <cesar4a6z@gmail.com>
2023-03-01 15:42:22 -0500

    Add Size Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Montserrat: ['Montserrat', 'sans-serif'],
+      },
+      screens: {
+        sm: '640px', // small screens, mobile devices
+        md: '768px', // medium screens, tablets
+        lg: '1024px', // large screens, laptops
+        xl: '1280px', // extra-large screens, desktops
+      },
+    },
   },
   plugins: [
     plugin(({ addComponents }) => {
