215b3623a66f12ffef07219844866f19dda1df4b
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-03-02 15:23:22 -0300

    Make more style changes

--- src/pages/DeleteDoctor.js
+++ src/pages/DeleteDoctor.js
@@ -12,8 +12,8 @@ const DeleteDoctor = () => {
     dispatch(deleteDoctor(doctor));
   };
   return (
-    <div className="w-3/4 flex flex-col items-center bg-lime-500 text-white">
-      <div className="w-full md:w-1/2 pl-0.5 pt-8 md:pt-9 md:pl-0 bg-lime-500">
+    <div className="h-full md:w-full flex flex-col items-center bg-lime-500 text-white">
+      <div className="w-full pl-0.5 pt-8 md:pt-9 md:pl-0 bg-lime-500">
         <h1 className="text-left pb-3 font-bold text-2xl">Delete Doctors:</h1>
         <ul className="p-1">
           {doctors.map((doctor) => (
