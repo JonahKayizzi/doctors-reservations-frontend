50870906f0c2cc0bb2dbbcf0250a46561f79b1f6
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-03-03 19:52:41 -0300

    Modify tests to work without sending requests

--- src/__test__/Login.test.js
+++ src/__test__/Login.test.js
@@ -1,13 +1,25 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import fetchMock from 'jest-fetch-mock';
 import { fetchUser } from '../redux/users';
 
-const mockStore = configureMockStore([thunk]);
+fetchMock.enableMocks();
+
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('fetchUser', () => {
   it('should fetch a user', async () => {
     const store = mockStore({});
+    const response = {
+      id: 1,
+    };
+    fetch.mockResponse(JSON.stringify(response));
     await store.dispatch(fetchUser());
     const actions = store.getActions();
     expect(actions[0].type).toEqual('users/fetchUser/pending');
