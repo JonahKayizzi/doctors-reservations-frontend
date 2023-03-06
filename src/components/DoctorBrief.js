abae1fe62888cfb1568279a2b55888ef82380d88
Camilovelag <camilo.vela.galindo@gmail.com>
2023-03-06 11:09:07 -0500

    Add changes: remove doctorBrief comment, fix carousel layout

--- src/components/DoctorBrief.js
+++ src/components/DoctorBrief.js
@@ -6,16 +6,18 @@ import { Link } from 'react-router-dom';
 import {
   TiSocialInstagram, TiSocialTwitter, TiSocialFacebook,
 } from 'react-icons/ti';
+import doctorBg from '../assets/doctor-brief-bg.png';
 
 const DoctorBrief = (props) => {
   const {
-    id, name, image, speciality, description,
+    id, name, image, speciality,
   } = props;
 
   return (
     <div className="flex flex-col relative h-full">
-      <div className="flex flex-col items-center">
-        <Link to={`/doctor/${id}`} className="hover:opacity-80 hover:scale-110 hover:duration-500">
+      <img src={doctorBg} alt="sitting area" />
+      <div className="absolute flex flex-col items-center pt-4 w-full h-full justify-between">
+        <Link to={`/doctor/${id}`} className="my-auto hover:opacity-80 hover:scale-110 hover:duration-500">
           <img src={image} alt="sitting area" />
         </Link>
         <div className="flex flex-col items-center pt-4 w-full gap-1 md:gap-2">
@@ -35,12 +37,6 @@ const DoctorBrief = (props) => {
           </div>
         </div>
       </div>
-      {/* Next div used to force carousel to work, do not remove */}
-      <div className="absolute flex flex-col items-center pt-4 w-full">
-        <p className="hidden text-gray-500">
-          { description.substring(0, 2) }
-        </p>
-      </div>
     </div>
   );
 };
@@ -50,7 +46,6 @@ DoctorBrief.propTypes = {
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   speciality: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
 };
 
 export default DoctorBrief;
