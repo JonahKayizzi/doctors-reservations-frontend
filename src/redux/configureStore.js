fe2fc795507b3af1f597337c3ce7a852d0ee6895
Camilovelag <camilo.vela.galindo@gmail.com>
2023-03-06 14:08:26 -0500

    Install redux_persist

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,15 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+
 import doctorsSlice from './doctors/doctorsSlice';
 import reservationsSlice from './reservations/reservationsSlice';
 import { usersSlice } from './users';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
 const reducer = combineReducers({
   users: usersSlice.reducer,
@@ -9,7 +17,9 @@ const reducer = combineReducers({
   reservationsReducer: reservationsSlice.reducer,
 });
 
-const store = configureStore({ reducer });
+const persistedReducer = persistReducer(persistConfig, reducer);
+const store = configureStore({ reducer: persistedReducer });
+const persistor = persistStore(store);
 
 export default store;
-export { reducer };
+export { persistor };
