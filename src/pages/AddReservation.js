eb7fe364218fc35c8f6aa2130705e49df1218f2c
Camilovelag <camilo.vela.galindo@gmail.com>
2023-03-06 13:45:55 -0500

    Fix add_appointment page bg bug

--- src/pages/AddReservation.js
+++ src/pages/AddReservation.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReservationForm from '../components/ReservationForm';
 
 const AddReservation = () => (
-  <div className="bg-form h-full flex flex-col items-center">
+  <div className="bg-form h-screen w-screen flex flex-col items-center">
     <div className="w-3/4 h-full flex flex-col items-center justify-center text-white">
       <h2 className="text-xl font-bold">BOOK A DOCTOR APPOINTMENT</h2>
       <div className="w-3/4 border-b-2 border-white-100 my-4" />
